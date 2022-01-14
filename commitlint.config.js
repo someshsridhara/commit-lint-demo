f1f01651ca2491f5697175c927b361a42ced79eb
Somesha Sridhara <ssridhara@woolworths.com.au>
2022-01-14 13:20:20 +1100

    [TEST-1] test: testing JIRA ID rule

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,24 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
-}
+    extends: ['@commitlint/config-conventional'],
+    parserPreset: {
+        parserOpts: {
+            headerPattern: /^(\[[A-Z]+-\d+] )?(\w*)(?:\((.*)\))?!?: (.*)$/,
+            breakingHeaderPattern: /^\[[A-Z]+-\d+](\w*)(?:\((.*)\))?!: (.*)$/,
+            headerCorrespondence: ['jira', 'type', 'scope', 'subject'],
+        },
+    },
+    rules: {
+        'contains-jira-id': [2, 'always'],
+        'scope-case': [2, 'always', ['lower-case', 'upper-case']],
+    },
+    plugins: [
+        {
+            rules: {
+                'contains-jira-id': ({ jira }) => [
+                    jira,
+                    'Your commit message must start with a JIRA issue ID in the form [PROJ-ID]',
+                ],
+            },
+        },
+    ],
+};
